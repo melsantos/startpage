15ce50bfbec9c7bae2a44474e64c4ef19924e73b
Melissa Santos <msant035@ucr.edu>
2021-09-04 01:03:58 -0700

    Added credit link. Darkened bg for visibility. Fixed grid issues

--- site.js
+++ site.js
@@ -1,15 +1,18 @@
 const API_KEY = 'PUT API KEY HERE'
 const CITY_ID = 'PUT CITY ID HERE'
+const SUBREDDIT = "https://www.reddit.com/r/EarthPorn/hot/"
+
+document.cookie = "promo_shown=1; Max-Age=2600000; Secure"
 
 function getTopPost() {
-  const EP = `https://www.reddit.com/r/EarthPorn/hot/.json?limit=3;raw_json=1`
-  var request = new Request(EP)
+  const site = `${SUBREDDIT}.json?limit=3;raw_json=1`
+  var request = new Request(site)
 
   fetch(request)  
     .then(response => response.json())
     .then(data => {
         // grab the hottest post of today
-        var posts = data.data.children;
+        var posts = data.data.children
         for (post of posts) {
             if (!post.data['stickied']) {
                 changeBackground(post)
@@ -22,20 +25,28 @@ function getTopPost() {
 function changeBackground(post) {
   var bg = document.getElementById("bg")
   var content = document.getElementById("content")
+  var credit = document.getElementById("credit")
+  var bgImg = new Image()
   
   try {
-    bg.style.backgroundImage = `url('${post.data['url']}')`
+    bgImg.src = `${post.data['url']}`
   }
   catch(e) {
     console.log(post)
-    bg.style.backgroundImage = "url('default.jpg')"
+    bgImg.src = "default.jpg"
   }
 
-  unfade(bg, 20)
-  unblur(bg, 20)
-  setTimeout(function() {
-    unfade(content, 5)
-  }, 750)
+  credit.innerHTML = `Photo Credit: u/${post.data['author']}`
+  credit.href = `https://www.reddit.com${post.data['permalink']}`
+
+  bgImg.onload = function() {
+    bg.style.backgroundImage = `url('${post.data['url']}')`
+    unfade(bg, 20)
+    unblur(bg, 20)
+    setTimeout(function() {
+      unfade(content, 5)
+    }, 770)
+  }
 }
 
 
@@ -43,26 +54,26 @@ function changeBackground(post) {
 /* https://creativecommons.org/licenses/by-sa/3.0/ */
 /* added unblur function */
 function unfade(element, speed) {
-  var op = 0.1;
+  var op = 0.1
   var opTimer = setInterval(function () {
       if (op >= 1){
-          clearInterval(opTimer);
+          clearInterval(opTimer)
       }
-      element.style.opacity = op;
-      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-      op += op * 0.1;
-  }, speed);
+      element.style.opacity = op
+      element.style.filter = 'alpha(opacity=' + op * 100 + ")"
+      op += op * 0.1
+  }, speed)
 }
 
 function unblur(element, speed) {
-  var blur = 30;
+  var blur = 30
   var blurTimer = setInterval(function () {
       if (blur <= 5){
-          clearInterval(blurTimer);
+          clearInterval(blurTimer)
       }
-      element.style.filter = "blur(" + blur + "px)"
-      blur -= 1;
-  }, speed);
+      element.style.filter = "blur(" + blur + "px) brightness(50%)"
+      blur -= 1
+  }, speed)
 }
 
 function getWeather() {
@@ -81,26 +92,33 @@ function getWeather() {
 
 function getTime() {
   var time = document.getElementById("time")
-  var date = document.getElementById("date")
-  var days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
 
   var dt = new Date()
   var hours = (dt.getHours() % 12 == 0) ? pad(12) : pad(dt.getHours() % 12)
   var mins = pad(dt.getMinutes())
-  var sec = pad(dt.getSeconds())
   var ampm = (dt.getHours() / 12 >= 1) ? "PM" : "AM"
+  
+  time.innerHTML = `${hours}:${mins}&nbsp${ampm}`
+}
+
+function getDate() {
+  var date = document.getElementById("date")
+  var days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+
+  var dt = new Date()
   var month = pad(dt.getMonth() + 1)
   var numDate = pad(dt.getDate())
   var year = dt.getFullYear()
-  
-  time.innerHTML = `${hours}:${mins}:${sec}&nbsp${ampm}`
-  date.innerHTML = `${days[dt.getDay()]}&nbsp${month}-${numDate}-${year}`
 
-  function pad(num) {
-    return num.toString().padStart(2, '0')
-  }
+  date.innerHTML = `${days[dt.getDay()]}&nbsp${month}-${numDate}-${year}`
 }
 
+function pad(num) {
+  return num.toString().padStart(2, '0')
+}
+
+getTime()
 setInterval(getTime, 1000)
-getTopPost()
+getDate()
 getWeather()
+getTopPost()
